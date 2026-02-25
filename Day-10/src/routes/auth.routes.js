ebfd0af5ba361106f72f7428c528e5142acacce8
CodeWith-Het <hetp1065@gmail.com>
2026-02-25 08:02:26 -0600

    feat: implement user registration and login with hashed passwords

--- Day-10/src/routes/auth.routes.js
+++ Day-10/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const jwt = require("jsonwebtoken")
 const userSchemaModels = require("../models/user.models")
+const crypto = require("crypto");
 
 const authRouter = express.Router()
 
@@ -13,12 +14,14 @@ authRouter.post("/register",async (req, res) => {
         res.status(409).json({
             message:"This Email Account already exist"
         })
-    }
+  }
+  
+  const hash =crypto.createHash("md5").update(password).digest("hex")
 
     const userData = await userSchemaModels.create({
       name,
       email,
-      password,
+      password: hash
     });
 
     const token = jwt.sign(
@@ -46,5 +49,41 @@ authRouter.post("/protected", (req, res) => {
     message:"this is protected Account"
   })
 });
+
+authRouter.post("/login", async(req, res) => {
+  const { email, password } = req.body
+  
+  const user = await userSchemaModels.findOne({ email })
+  
+  if (!user) {
+    return res.status(404).json({
+      messgae:"user is not found"
+    })
+  }
+
+  const isPassword = user.password === crypto.createHash("md5").update(password).digest("hex")
+
+  if (!isPassword) {
+    return res.status(401).json({
+      message:"User Password id worng"
+    })
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+  );
+  
+   res.cookie("jwt_token", token);
+  
+  res.status(200).json({
+    message: "User Is Loggin",
+    user,
+    token
+  })
+})
 
 module.exports=authRouter
